580c366ddfc1758389b53e37e2ca0062fdc4d43f
Prashik Patil <70844686+prashikpatil221@users.noreply.github.com>
2021-06-18 18:47:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/player/Model2.java
@@ -0,0 +1,22 @@
+package com.example.player;
+
+public class Model2 {
+    String name,url;
+    public Model2(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
